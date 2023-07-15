b8c79bf8cc9d409115747fa7552b7bf542a2dbb9
zolar5000 <gabzolar5000@gmail.com>
2023-07-15 09:16:27 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ *
+ * Return: Always 0 (good)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+Return(0);
+}
